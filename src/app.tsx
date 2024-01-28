8eb0d6d2cfa59724f8bd6a86cefffe62dd39caa8
seokhochoi <seokho9473@gmail.com>
2024-01-28 22:23:00 +0900

    CloudFront HTTP 설정 변경

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import styles from "./app.module.scss";
 
 export default function App() {
-  return <div className={styles.renderTest}>app - test - 2</div>;
+  return <div className={styles.renderTest}>app - test</div>;
 }
