10ab8d5451cfcf3169adf173bbbd92acf323fc96
seokhochoi <seokho9473@gmail.com>
2024-02-08 23:36:14 +0900

    feat(#27): 검색을 위한 파라미터 추가

--- src/api/alliniApi.ts
+++ src/api/alliniApi.ts
@@ -3,7 +3,7 @@ import HttpClient from "./httpClient";
 export default class AlliniApi {
   constructor(private httpClient: HttpClient) {}
 
-  async searchSnack() {
-    return this.httpClient.get("/posts");
+  async searchSnack(keyword = "") {
+    return this.httpClient.get(`/posts/${keyword}`);
   }
 }
