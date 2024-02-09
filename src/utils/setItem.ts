2f2324d5913a19d5e4cd6650899aeaf073c8cb51
seokhochoi <seokho9473@gmail.com>
2024-02-10 01:51:44 +0900

    feat(#27): 간단한 로컬스토리지 처리를 위한 setter

--- src/utils/setItem.ts
+++ src/utils/setItem.ts
@@ -1,4 +1,9 @@
-export const setItem = (key: string, value: unknown) => {
+export const setItem = (key: string, value: any) => {
+  if (value === "") {
+    localStorage.setItem(key, value);
+    return;
+  }
+
   const stringified = JSON.stringify(value);
   localStorage.setItem(key, stringified);
 };
