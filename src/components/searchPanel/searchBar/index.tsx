cc8eceae6cdb7cd42202561cdde449ea682c1859
seokhochoi <seokho9473@gmail.com>
2024-02-10 02:44:00 +0900

    feat(#27): 검색창 컴포넌트

--- src/components/searchPanel/searchBar/index.tsx
+++ src/components/searchPanel/searchBar/index.tsx
@@ -1,3 +1,34 @@
-export default function SearchBar() {
-  return <div>SearchBar</div>;
+import { useEffect } from "react";
+
+interface Props {
+  keyword: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onFocus: () => void;
+}
+
+export default function SearchBar({ keyword, onChange, onFocus }: Props) {
+  return (
+    <fieldset>
+      <legend>검색</legend>
+      <span>
+        <label htmlFor="query">
+          <input
+            type="text"
+            title="Allini 검색어 입력"
+            id="query"
+            name="query"
+            maxLength={80}
+            autoComplete="off"
+            onFocus={onFocus}
+            onChange={onChange}
+            value={keyword}
+            onClick={(e) => e.stopPropagation()}
+          />
+          <button role="img" aria-label="검색">
+            🔍
+          </button>
+        </label>
+      </span>
+    </fieldset>
+  );
 }
