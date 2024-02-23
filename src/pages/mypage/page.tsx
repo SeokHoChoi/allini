59012cbebfa874219eb4635dfb5173353b033698
seokhochoi <seokho9473@gmail.com>
2024-02-24 01:32:55 +0900

    feat(#31): 2차 완성된 Select 컴포넌트 사용 예시

--- src/pages/mypage/page.tsx
+++ src/pages/mypage/page.tsx
@@ -1,4 +1,26 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import Select from "../../components/select";
+
 export default function Mypage() {
   // 내 정보 수정, 등록된 사료/간식, 일간/월간, 즐겨찾기 -> 이렇게 네 개의 탭으로 나눌 예정.
-  return <div>Mypage</div>;
+  const [value, setValue] = useState<string | number>(2);
+  function handleSelectedValue(e: ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value);
+  }
+  useEffect(() => {
+    console.log(value, " 값변함ㄴㅌ");
+  }, [value]);
+  return (
+    <div>
+      <Select value={value} onChange={handleSelectedValue}>
+        <Select.Label>사료/간식</Select.Label>
+        <Select.Option>골라주세요!</Select.Option>
+        <Select.Option value={1}>안녕1</Select.Option>
+        <Select.Option value={2} disabled>
+          안녕2
+        </Select.Option>
+        <Select.Option value={3}>안녕3</Select.Option>
+      </Select>
+    </div>
+  );
 }
