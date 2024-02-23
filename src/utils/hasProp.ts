f963f390be8056500f2b8a73e8956772faae2e38
seokhochoi <seokho9473@gmail.com>
2024-02-24 01:31:43 +0900

    style(#31): 간편한 사용을 위해 타입 가드 함수를 추출했습니다.

--- src/utils/hasProp.ts
+++ src/utils/hasProp.ts
@@ -1,5 +1,5 @@
 /**
- * @description  제공된 객체에 특정 속성이 있는지 확인하는  타입  가드 함수입니다.
+ * @description  제공된 객체에 특정 속성이 있는지 확인하는 타입 가드 함수입니다.
  * @param obj 확인할 객체
  * @param propName 확인할 속성  이름
  * @returns 속성이 있는  경우 true, 없는  경우 false
