582c0b77a19f54072d1e7c00c969af61259edd38
seokhochoi <seokho9473@gmail.com>
2024-05-30 17:15:08 +0900

    refactor(#43): 사료/간식 과 강아지 입력정보 탭으로 분리
    입력내용이 너무 길어지는 상황을 방지,
    한 마리의 강아지가 등록된 상태라면 자동적용 예정

--- src/pages/meal-registration/index.tsx
+++ src/pages/meal-registration/index.tsx
@@ -1,41 +1,35 @@
 import { useState } from "react";
-import ListItem from "../../components/customSelect/listItem";
-import ListModal from "../../components/customSelect/listModal";
-
-const options = [
-  { id: 1, value: " 사료" },
-  { id: 2, value: "간식" },
-  { id: 3, value: "임시" },
-];
+import Meal from "./meal";
+import Puppy from "./puppy";
 
 /** v1 에선 사료/간식은 동일한 데이터를 받을 예정입니다. */
 export default function MealRegistration() {
-  const [feed, setFeed] = useState("food");
+  const [isMealTab, setIsMealTab] = useState(true);
+  const selectedTab = isMealTab ? <Meal /> : <Puppy />;
 
   return (
     <div>
-      {/* 사료 or 간식 */}
-
-      <ListModal value={feed} onChagne={(feed) => setFeed(feed)}>
-        <ListItem value="" defaultOptionMessage>
-          사료/간식 - 선택해 주세요!
-        </ListItem>
-        <ListItem value="food">사료</ListItem>
-        <ListItem value="snack">간식</ListItem>
-        <ListItem value="temp">임시</ListItem>
-      </ListModal>
-      {/* 대표 이미지 설정 - 없을시 화이트 슈나우져 그림이나 사진 보여주기 */}
-      {/* 간식명 */}
-      {/* 브랜드명 */}
-      {/* 금액 - 셀렉트 창으로 범위로써 고르게 할까? */}
-      {/* 구매한 장소 및 사이트 */}
-      {/* 들어가는 재료, [이미지, 이미지에서 뽑은 데이터, 내가 직접 적은 데이터] */}
-      {/* 본문 - 그냥 기록용 */}
-      {/* 알레르기 정도: 없음, 보통, 심함 */}
-      {/* 선호도: 싫어한다, 관심이 없다, 좋아한다. */}
-      {/* 먹은 날짜 및 시간 직접 입력 */}
-      {/* 먹고나서 어떤 특이사항이 있는지 셀렉트창: 구토, 가려움, 쇼크, 이상없음, 켁켁거림, 기타(직접 입력) 등등.. */}
-      {/* 즐겨 찾기 - 최애 사료, 최애 간식 느낌! */}
+      <nav role="tablist" aria-label="Sample Tabs">
+        <button
+          role="tab"
+          aria-selected={isMealTab} // 현재 선택된 탭을 식별하는 데 사용
+          id="tab-meal"
+          onClick={() => setIsMealTab(true)}
+          aria-controls="panel-meal" // 탭 버튼이 제어하는 탭 패널을 식별
+        >
+          상품 정보
+        </button>
+        <button
+          role="tab"
+          aria-selected={!isMealTab}
+          id="tab-puppy"
+          onClick={() => setIsMealTab(false)}
+          aria-controls="panel-puppy"
+        >
+          강아지 정보
+        </button>
+      </nav>
+      <div>{selectedTab}</div>
     </div>
   );
 }
