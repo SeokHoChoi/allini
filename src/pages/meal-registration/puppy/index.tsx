a2683cf3653bbf07e4448c0b175546150776e02b
seokhochoi <seokho9473@gmail.com>
2024-05-30 17:07:57 +0900

    feat(#43): 간식/사료 정보와 섭취한 강아지 정보 컴포넌트 분리

---
+++ src/pages/meal-registration/puppy/index.tsx
@@ -0,0 +1,8 @@
+export default function Puppy() {
+  return (
+    <section role="tabpanel" id="panel-puppy" aria-labelledby="tab-puppy">
+      <h2>puppy</h2>
+      <p>강아지 정보를 등록하자!</p>
+    </section>
+  );
+}
