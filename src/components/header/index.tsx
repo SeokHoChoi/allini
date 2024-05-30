683305ff15fe89af034fc7577893e1e778c3ef8c
seokhochoi <seokho9473@gmail.com>
2024-05-30 14:17:06 +0900

    refactor(#41): 새로운 시안으로 수정, 기본 헤더 사용페이지 소통 전.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,54 +1,32 @@
 import { Link } from "react-router-dom";
-import LOGO from "../../assets/image/allini/testimg.jpeg";
+import styles from "./index.module.scss";
+import LOGO from "../../assets/image/allini/logo_header.png";
+import ALLINI from "../../assets/image/allini/allini_header.png";
+import SEARCH from "../../assets/image/etc/search.png";
+import MENU from "../../assets/image/etc/hamburger_menu.png";
+import HeaderLayout from "../../layout/headerLayout";
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
+    <HeaderLayout>
+      <li>
+        <h1>
+          <Link className={styles.logoWrapper} to="/">
+            <img src={LOGO} alt="Allini" />
+            <img src={ALLINI} alt="알리니" />
+          </Link>
+        </h1>
+      </li>
+      <li>
+        <ul className={styles.mobileMenuWrapper}>
           <li>
-            <h1>
-              <Link to="/">
-                <img src={LOGO} alt="Allini" width="50px" height="50px" />
-              </Link>
-            </h1>
+            <img src={SEARCH} alt="검색" />
           </li>
           <li>
-            <ul>
-              <li>
-                <Link to="/snack-list">간식</Link>
-              </li>
-              <li>
-                <Link to="/food-list">사료</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            {false ? (
-              <ul>
-                <li>
-                  <button>로그인</button>
-                </li>
-                <li>
-                  <Link to="/signup">회원가입</Link>
-                </li>
-              </ul>
-            ) : (
-              <ul>
-                <li>
-                  <button>로그아웃</button>
-                </li>
-                <li>
-                  <Link to="/mypage">마이페이지</Link>
-                </li>
-                <li>
-                  <Link to="/meal-registration">사료/간식 등록</Link>
-                </li>
-              </ul>
-            )}
+            <img src={MENU} alt="메뉴" />
           </li>
         </ul>
-      </nav>
-    </header>
+      </li>
+    </HeaderLayout>
   );
 }
