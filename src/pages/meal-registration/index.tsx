bbe30d7be08b7e90cb34ba02bba9f781b5a01ca8
seokhochoi <seokho9473@gmail.com>
2024-06-01 21:36:15 +0900

    feat(#43): 탭키 UI 완성

--- src/pages/meal-registration/index.tsx
+++ src/pages/meal-registration/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Meal from "./meal";
 import Puppy from "./puppy";
+import styles from "./index.module.scss";
 
 /** v1 에선 사료/간식은 동일한 데이터를 받을 예정입니다. */
 export default function MealRegistration() {
@@ -10,11 +11,12 @@ export default function MealRegistration() {
   return (
     <div>
       <nav role="tablist" aria-label="Sample Tabs">
-        <ul>
-          <li role="presentation">
+        <ul className={styles.tabWrapper}>
+          <li role="presentation" className={styles.tab}>
             <button
               role="tab"
               aria-selected={isMealTab} // 현재 선택된 탭을 식별하는 데 사용
+              className={isMealTab ? styles.selected : ""}
               id="tab-meal"
               onClick={() => setIsMealTab(true)}
               aria-controls="panel-meal" // 탭 버튼이 제어하는 탭 패널을 식별
@@ -22,10 +24,11 @@ export default function MealRegistration() {
               상품 정보
             </button>
           </li>
-          <li role="presentation">
+          <li role="presentation" className={styles.tab}>
             <button
               role="tab"
               aria-selected={!isMealTab}
+              className={!isMealTab ? styles.selected : ""}
               id="tab-puppy"
               onClick={() => setIsMealTab(false)}
               aria-controls="panel-puppy"
