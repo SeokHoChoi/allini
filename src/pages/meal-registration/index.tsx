174ccaff2d6fc4f87227f7ff71657b9ebd1b6b51
seokhochoi <seokho9473@gmail.com>
2024-06-01 22:42:25 +0900

    Feat: 조건적 SCSS Class 사용의 가독성 위해 classnames 라이브러리 도입

--- src/pages/meal-registration/index.tsx
+++ src/pages/meal-registration/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Meal from "./meal";
 import Puppy from "./puppy";
+import classNames from "classnames/bind";
 import styles from "./index.module.scss";
 
 /** v1 에선 사료/간식은 동일한 데이터를 받을 예정입니다. */
@@ -8,15 +9,17 @@ export default function MealRegistration() {
   const [isMealTab, setIsMealTab] = useState(true);
   const selectedTab = isMealTab ? <Meal /> : <Puppy />;
 
+  const cx = classNames.bind(styles);
+
   return (
     <div>
       <nav role="tablist" aria-label="Sample Tabs">
-        <ul className={styles.tabWrapper}>
+        <ul className={cx("tabWrapper")}>
           <li role="presentation">
             <button
               role="tab"
               aria-selected={isMealTab} // 현재 선택된 탭을 식별하는 데 사용
-              className={isMealTab ? styles.selected : ""}
+              className={cx({ selected: isMealTab })}
               id="tab-meal"
               onClick={() => setIsMealTab(true)}
               aria-controls="panel-meal" // 탭 버튼이 제어하는 탭 패널을 식별
@@ -24,11 +27,11 @@ export default function MealRegistration() {
               상품 정보
             </button>
           </li>
-          <li role="presentation" className={styles.tab}>
+          <li role="presentation" className={cx("tab")}>
             <button
               role="tab"
               aria-selected={!isMealTab}
-              className={!isMealTab ? styles.selected : ""}
+              className={cx({ selected: !isMealTab })}
               id="tab-puppy"
               onClick={() => setIsMealTab(false)}
               aria-controls="panel-puppy"
@@ -39,9 +42,10 @@ export default function MealRegistration() {
         </ul>
       </nav>
       <article
-        className={`${styles.panelArea} ${
-          isMealTab ? styles.mealPanel : styles.puppyPanel
-        }`}
+        className={cx("panelArea", {
+          mealPanel: isMealTab,
+          puppyPanel: !isMealTab,
+        })}
       >
         {selectedTab}
       </article>
