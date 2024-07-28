a3f2c8485e70e2c816a9e1c53efee3e2732c9346
seokhochoi <seokho9473@gmail.com>
2024-07-28 20:48:44 +0900

    feat: 임시 커밋

--- src/pages/meal-registration/puppy/index.tsx
+++ src/pages/meal-registration/puppy/index.tsx
@@ -1,7 +1,7 @@
 export default function Puppy() {
   return (
     <section role="tabpanel" id="panel-puppy" aria-labelledby="tab-puppy">
-      <h2>puppy</h2>
+      <h2>강아지 정보</h2>
       <p>강아지 정보를 등록하자!</p>
     </section>
   );
