ee003df36538f1f33aadcaad61a5af19eb764b7c
seokhochoi <seokho9473@gmail.com>
2024-08-12 17:53:52 +0900

    feat: Implement layout-based routing to conditionally include Header and Footer

--- src/shared/layouts/defaultLayout/index.tsx
+++ src/shared/layouts/defaultLayout/index.tsx
@@ -1,12 +1,16 @@
 import Footer from "@widgets/footer";
 import Header from "@widgets/header";
 import styles from "./index.module.scss";
+import { Outlet } from "react-router-dom";
 
-export default function DefaultLayout({ children }) {
+export default function DefaultLayout() {
   return (
     <div className={styles.layoutArea}>
       <Header />
-      <main className={styles.mainArea}>{children}</main>
+      <main className={styles.mainArea}>
+        {" "}
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
