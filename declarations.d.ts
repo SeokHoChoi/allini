be6c42e6b7f297bd84ab0547cefaa18c461dc2c7
seokhochoi <seokho9473@gmail.com>
2024-10-03 21:39:35 +0900

    chore: SVG 타입 설정

--- declarations.d.ts
+++ declarations.d.ts
@@ -2,6 +2,19 @@ declare module "*.module.scss" {
   const classes: { [key: string]: string };
   export default classes;
 }
+declare module "*.svg?url" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.svg" {
+  import React = require("react");
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
 declare module "*.jpeg";
 declare module "*.jpg";
 declare module "*.png";
