e9a813499965ed11e8ce45faadf70b8477c1cc14
seokhochoi <seokho9473@gmail.com>
2024-10-08 20:46:52 +0900

    feat(#53): 웹뷰 핵심화면이 375px 이하 해상도만을 대응하기 위해 데스크탑 헤더 및 분기로직 제거

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -4,12 +4,8 @@ import LOGO from "@images/allini/logo_header.png";
 import ALLINI from "@images/allini/allini_header.png";
 import HeaderLayout from "@layouts/headerLayout";
 import MobileHeader from "./mobileHeader";
-import DesktopHeader from "./desktopHeader";
-import { useResponsive } from "@hooks/useResponsive";
 
 export default function Header() {
-  const isMobile = useResponsive();
-
   return (
     <HeaderLayout>
       <li>
@@ -20,7 +16,9 @@ export default function Header() {
           </Link>
         </h1>
       </li>
-      <li>{isMobile ? <MobileHeader /> : <DesktopHeader />}</li>
+      <li>
+        <MobileHeader />
+      </li>
     </HeaderLayout>
   );
 }
