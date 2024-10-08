d025e34d8296277caff2435f60d3bc544575eaa0
seokhochoi <seokho9473@gmail.com>
2024-10-08 21:25:23 +0900

    feat(#53): 드롭다운을 위한 Sidebar Import 위치 변경

--- src/shared/layouts/headerLayout/index.tsx
+++ src/shared/layouts/headerLayout/index.tsx
@@ -1,9 +1,18 @@
+import Sidebar from "@widgets/header/sidebar";
 import styles from "./index.module.scss";
+import { useSidebar } from "@contexts/sidebarContext";
 
 export default function HeaderLayout({ children }) {
+  const { isOpen } = useSidebar().state;
+
   return (
     <header className={styles.area}>
       <ul className={styles.listWrapper}>{children}</ul>
+      {isOpen && (
+        <li>
+          <Sidebar />
+        </li>
+      )}
     </header>
   );
 }
--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -4,12 +4,8 @@ import LOGO from "@images/allini/logo_header.png";
 import ALLINI from "@images/allini/allini_header.png";
 import HeaderLayout from "@layouts/headerLayout";
 import MobileHeader from "./mobileHeader";
-import { useSidebar } from "@contexts/sidebarContext";
-import Sidebar from "./sidebar";
 
 export default function Header() {
-  const { isOpen } = useSidebar().state;
-
   return (
     <HeaderLayout>
       <li>
@@ -23,11 +19,6 @@ export default function Header() {
       <li>
         <MobileHeader />
       </li>
-      {isOpen && (
-        <li>
-          <Sidebar />
-        </li>
-      )}
     </HeaderLayout>
   );
 }
