4df898bef497ad37a9f63e7c85305e22c5db72f1
seokhochoi <seokho9473@gmail.com>
2024-10-08 23:53:36 +0900

    feat(#53): 페이지 이동시 Sidebar 액션 로직 구현

--- src/widgets/header/sidebar/index.tsx
+++ src/widgets/header/sidebar/index.tsx
@@ -2,18 +2,26 @@ import classnames from "classnames/bind";
 import styles from "./index.module.scss";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
+import { useSidebar } from "@contexts/sidebarContext";
 
 const cx = classnames.bind(styles);
 
 export default function Sidebar({ className }: { className?: string }) {
+  const { setIsOpen } = useSidebar().actions;
   const [isRecordOpen, setRecordOpen] = useState(false);
+  const [recordActive, setRecordActive] = useState(false);
 
   const toggleRecord = () => {
     setRecordOpen((prev) => !prev);
   };
 
+  const handleToggle = (isOpen = false) => {
+    setIsOpen(false);
+    setRecordOpen(isOpen);
+  };
+
   return (
-    <ul className={cx("sidebar", className)}>
+    <ul className={cx("sidebar", className)} onClick={() => handleToggle(true)}>
       <li>
         <NavLink
           to="/about"
@@ -31,28 +39,51 @@ export default function Sidebar({ className }: { className?: string }) {
         </NavLink>
       </li>
       <li>
-        <button onClick={toggleRecord} className={styles.recordButton}>
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            if (recordActive) {
+              return;
+            }
+            toggleRecord();
+          }}
+          className={styles.recordButton}
+        >
           알리니 기록
         </button>
       </li>
-      <ul className={cx("subList", { open: isRecordOpen })}>
-        <li>
-          <NavLink
-            to="/food-tracker/today"
-            className={({ isActive }) => cx({ selected: isActive })}
-          >
-            오늘의 기록하기
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/food-tracker/report"
-            className={({ isActive }) => cx({ selected: isActive })}
-          >
-            기록 통계
-          </NavLink>
-        </li>
-      </ul>
+      {isRecordOpen && (
+        <ul
+          className={cx("subList", { open: isRecordOpen })}
+          onClick={(e) => {
+            e.stopPropagation();
+            handleToggle(true);
+          }}
+        >
+          <li>
+            <NavLink
+              to="/food-tracker/today"
+              className={({ isActive }) => {
+                setRecordActive(true);
+                return cx({ selected: isActive });
+              }}
+            >
+              오늘의 기록하기
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/food-tracker/report"
+              className={({ isActive }) => {
+                setRecordActive(true);
+                return cx({ selected: isActive });
+              }}
+            >
+              기록 통계
+            </NavLink>
+          </li>
+        </ul>
+      )}
       <li>
         <NavLink
           to="/mypage"
