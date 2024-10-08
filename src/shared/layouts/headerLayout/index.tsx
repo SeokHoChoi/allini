3fef424633e2057f42c2287616523083a5fd46b6
seokhochoi <seokho9473@gmail.com>
2024-10-08 21:45:57 +0900

    feat(#53): Sidebar 컴포넌트에 아코디언 기능 추가

--- src/shared/layouts/headerLayout/index.tsx
+++ src/shared/layouts/headerLayout/index.tsx
@@ -1,18 +1,19 @@
+import classnames from "classnames/bind";
 import Sidebar from "@widgets/header/sidebar";
 import styles from "./index.module.scss";
 import { useSidebar } from "@contexts/sidebarContext";
+
+const cx = classnames.bind(styles);
 
 export default function HeaderLayout({ children }) {
   const { isOpen } = useSidebar().state;
 
   return (
-    <header className={styles.area}>
-      <ul className={styles.listWrapper}>{children}</ul>
-      {isOpen && (
-        <li>
-          <Sidebar />
-        </li>
-      )}
+    <header className={cx("area")}>
+      <ul className={cx("listWrapper")}>{children}</ul>
+      <li>
+        <Sidebar className={cx({ open: isOpen })} />
+      </li>
     </header>
   );
 }
