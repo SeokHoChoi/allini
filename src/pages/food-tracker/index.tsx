673d91e95dc83e63300819fa522e1e09bf43e148
seokhochoi <seokho9473@gmail.com>
2024-10-14 20:35:45 +0900

    feat(#57): 관심사를 분리한 TabComponent를 FoodTracker 페이지 컴포넌트에 적용

--- src/pages/food-tracker/index.tsx
+++ src/pages/food-tracker/index.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import TabComponent from "@ui/tabComponent";
+
+const tabs = [
+  { path: "today", label: "오늘의 밥그릇" },
+  { path: "food-treats", label: "밥그릇 모아보기" },
+  { path: "report", label: "밥그릇 기록장" },
+];
 
 export default function FoodTracker() {
-  return (
-    <>
-      <h1>FoodTracker Component</h1>
-      <Outlet />
-    </>
-  );
+  return <TabComponent tabs={tabs} />;
 }
