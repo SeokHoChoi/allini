b452c2a93f930f23a3c5fa92a8dabb4c88c7b0d7
seokhochoi <seokho9473@gmail.com>
2024-10-14 20:32:46 +0900

    feat(#57): tab 컴포넌트 재사용을 위해 register 페이지 컴포넌트에서 TabComponent 분리

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,44 +1,11 @@
-import clsx from "clsx";
-import { NavLink, Outlet } from "react-router-dom";
-import styles from "./index.module.scss";
+import TabComponent from "@ui/tabComponent";
+
+const tabs = [
+  { path: "food", label: "상품 정보" },
+  { path: "puppy", label: "강아지 정보" },
+];
 
 /** v1 에선 사료/간식은 동일한 데이터를 받을 예정입니다. */
 export default function Registration() {
-  return (
-    <div>
-      <nav role="tablist" aria-label="Sample Tabs">
-        <ul className={clsx(styles.tabWrapper)}>
-          <li role="presentation">
-            <NavLink
-              to="food"
-              role="tab"
-              className={({ isActive }) =>
-                clsx({ [styles.selected]: isActive })
-              }
-              id="tab-food"
-              aria-controls="panel-food" // 탭 버튼이 제어하는 탭 패널을 식별
-            >
-              상품 정보
-            </NavLink>
-          </li>
-          <li role="presentation" className={clsx(styles.tab)}>
-            <NavLink
-              to="puppy"
-              role="tab"
-              className={({ isActive }) =>
-                clsx({ [styles.selected]: isActive })
-              }
-              id="tab-puppy"
-              aria-controls="panel-puppy"
-            >
-              강아지 정보
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <article className={clsx(styles.panelArea)}>
-        <Outlet />
-      </article>
-    </div>
-  );
+  return <TabComponent tabs={tabs} />;
 }
