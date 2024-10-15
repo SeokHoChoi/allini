e02104379d5e338921a988b833afd64a5f3a6875
seokhochoi <seokho9473@gmail.com>
2024-10-15 11:27:17 +0900

    feat(#57): Today 컴포넌트 동산 배경 구현

--- src/pages/food-tracker/today/index.tsx
+++ src/pages/food-tracker/today/index.tsx
@@ -1,3 +1,17 @@
+import clsx from "clsx";
+import styles from "./index.module.scss";
+
 export default function Today() {
-  return <div>Today</div>;
+  return (
+    <div className={clsx(styles.todayArea)}>
+      <div className={clsx(styles.background)}></div>
+      <div className={clsx(styles.hill)}></div>
+
+      <div className={clsx(styles.content)}>
+        <div>몽실이/복실이</div>
+        <div>강아지 사진, 밥그릇</div>
+        <div>사료먹기 / 간식먹기</div>
+      </div>
+    </div>
+  );
 }
