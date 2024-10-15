e8b7703a8f9ca9d73302867ba849162c6d6f7d15
seokhochoi <seokho9473@gmail.com>
2024-10-15 11:36:20 +0900

    feat(#57): svg 표지판 추가 및 높이 설정

--- src/pages/food-tracker/today/index.tsx
+++ src/pages/food-tracker/today/index.tsx
@@ -1,11 +1,13 @@
 import clsx from "clsx";
 import styles from "./index.module.scss";
+import Sign from "@assets/icons/dog-heart-sign.svg";
 
 export default function Today() {
   return (
     <div className={clsx(styles.todayArea)}>
-      <div className={clsx(styles.background)}></div>
+      <Sign className={clsx(styles.sign)} />
       <div className={clsx(styles.hill)}></div>
+      <div className={clsx(styles.background)}></div>
 
       <div className={clsx(styles.content)}>
         <div>몽실이/복실이</div>
