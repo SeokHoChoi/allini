62bba0d8fec4e78af74458ddba5e05e84b655664
seokhochoi <seokho9473@gmail.com>
2024-10-16 19:34:34 +0900

    feat(#59): 화면 조정용 미디어쿼리 추가

--- src/shared/layouts/headerLayout/index.tsx
+++ src/shared/layouts/headerLayout/index.tsx
@@ -6,9 +6,7 @@ export default function HeaderLayout({ children }) {
   return (
     <header className={clsx(styles.area)}>
       <ul className={clsx(styles.listWrapper)}>{children}</ul>
-      <li>
-        <Sidebar />
-      </li>
+      <Sidebar />
     </header>
   );
 }
