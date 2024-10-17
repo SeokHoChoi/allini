65a85ebab702894a13302becb35ea4b61a506dac
seokhochoi <seokho9473@gmail.com>
2024-10-17 21:10:05 +0900

    feat(#59): clsx 문법으로 수정

--- src/shared/ui/badge/index.tsx
+++ src/shared/ui/badge/index.tsx
@@ -1,10 +1,11 @@
+import clsx from "clsx";
 import styles from "./index.module.scss";
 
 export default function Badge({ labels }: { labels: string[] }) {
   return (
-    <div className={styles.badges}>
+    <div className={clsx(styles.badges)}>
       {labels.map((label, index) => (
-        <span key={index} className={styles.badge}>
+        <span key={index} className={clsx(styles.badge)}>
           {label}
         </span>
       ))}
