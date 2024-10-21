7d9cd4aa23dc25de03fd2dd776af0275afb90c02
seokhochoi <seokho9473@gmail.com>
2024-10-21 19:06:28 +0900

    feat(#61): 달력의 Open/Close 버전 UI 구현 및 분기

--- src/widgets/report/fullCalendar/index.tsx
+++ src/widgets/report/fullCalendar/index.tsx
@@ -1,16 +1,32 @@
 import CalendarCore from "./calendar";
 import clsx from "clsx";
 import styles from "./index.module.scss";
+import { useState } from "react";
 
 export default function Calendar() {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const toggleCalendar = () => {
+    setIsCollapsed((prev) => !prev);
+  };
+
   return (
-    <CalendarCore
-      className={clsx(styles.customCalendar)}
-      events={[
-        { title: "Test Event 1 - This is..", date: "2024-10-01" },
-        { title: "Event 2", date: "2024-10-07" },
-        { title: "Event 3", date: "2024-10-07" },
-      ]}
-    />
+    <>
+      <button onClick={toggleCalendar}>
+        {isCollapsed ? "Expand Calendar" : "Collapse Calendar"}
+      </button>
+      <CalendarCore
+        className={clsx(styles.customCalendar, {
+          [styles.collapsed]: isCollapsed,
+          [styles.calendarExpanded]: !isCollapsed,
+        })}
+        events={[
+          { title: "Test Event 1 - This is..", date: "2024-10-01" },
+          { title: "Event 2", date: "2024-10-07" },
+          { title: "Event 3", date: "2024-10-07" },
+        ]}
+        isCollapsed={isCollapsed}
+      />
+    </>
   );
 }
