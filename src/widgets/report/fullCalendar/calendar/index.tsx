4b79bfb20143d3004ef88e2aa55871d27550d0af
seokhochoi <seokho9473@gmail.com>
2024-10-21 19:06:28 +0900

    feat(#61): 달력의 Open/Close 버전 UI 구현 및 분기

--- src/widgets/report/fullCalendar/calendar/index.tsx
+++ src/widgets/report/fullCalendar/calendar/index.tsx
@@ -2,21 +2,41 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import clsx from "clsx";
 import { EventInput } from "@fullcalendar/core";
+import { useEffect, useRef, useState } from "react";
 
 interface CalendarCoreProps {
   events?: EventInput[];
   className?: string;
+  isCollapsed: boolean;
 }
+
+const DAY_NAMES = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
 export default function CalendarCore({
   events = [],
   className = "",
+  isCollapsed = false,
 }: CalendarCoreProps) {
+  const calendarRef = useRef<FullCalendar>(null);
+  const [currentView, setCurrentView] = useState<
+    "dayGridWeek" | "dayGridMonth"
+  >("dayGridWeek");
+
+  useEffect(() => {
+    const newView = isCollapsed ? "dayGridWeek" : "dayGridMonth";
+
+    if (calendarRef.current) {
+      setCurrentView(newView);
+      calendarRef.current.getApi().changeView(newView);
+    }
+  }, [isCollapsed]);
+
   return (
     <div className={clsx(className)}>
       <FullCalendar
+        ref={calendarRef}
         plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
+        initialView={currentView}
         headerToolbar={{
           left: "prev",
           center: "title",
@@ -24,14 +44,31 @@ export default function CalendarCore({
         }}
         events={events}
         dayCellContent={(arg) => {
-          return (
-            <div className={"dayCell"}>
-              {arg.dayNumberText.replace("일", "")}
-            </div>
-          );
+          if (!isCollapsed) {
+            return (
+              <div className={"dayCell"}>
+                {arg.dayNumberText.replace("일", "")}
+              </div>
+            );
+          }
+          return null;
         }}
-        dayMaxEventRows={1}
-        height="auto"
+        dayHeaderContent={(arg) => {
+          if (isCollapsed) {
+            const isToday = arg.isToday;
+            return (
+              <div className="custom-day-header">
+                <div>{DAY_NAMES[arg.date.getDay()]}</div>
+                <div className={clsx("dayCell", { today: isToday })}>
+                  {arg.date.getDate()}
+                </div>
+              </div>
+            );
+          }
+          return DAY_NAMES[arg.date.getDay()];
+        }}
+        dayMaxEventRows={isCollapsed ? 0 : 1}
+        height={isCollapsed ? "200px" : "auto"}
         locale="kr"
       />
     </div>
