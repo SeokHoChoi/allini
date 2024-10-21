35d399884f7b58bf6814e28446648bee14c65bfc
seokhochoi <seokho9473@gmail.com>
2024-10-21 21:33:42 +0900

    feat(#61): 캘린더 View 타입 변경버튼 구현 및 수치수정

--- src/pages/food-tracker/report/index.tsx
+++ src/pages/food-tracker/report/index.tsx
@@ -1,11 +1,13 @@
 import Calendar from "@widgets/report/fullCalendar";
+import clsx from "clsx";
+import styles from "./index.module.scss";
 
 export default function Report() {
   return (
-    <>
+    <div className={clsx(styles.reportArea)}>
       <Calendar />
       <div>바 차트 및 음식 추가</div>
       <div>성분 조회 데이터</div>
-    </>
+    </div>
   );
 }
