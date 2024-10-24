9d3e9845908694244a3187e663e61a9672b28e76
seokhochoi <seokho9473@gmail.com>
2024-10-24 22:13:33 +0900

    feat(#63): Z Fold 5 대응

--- src/pages/food-tracker/report/index.tsx
+++ src/pages/food-tracker/report/index.tsx
@@ -2,6 +2,7 @@ import Calendar from "@widgets/report/fullCalendar";
 import clsx from "clsx";
 import styles from "./index.module.scss";
 import WrapperBox from "@ui/wrapperBox";
+import FeedingStatus from "@widgets/report/feedIngStatus";
 
 export default function Report() {
   return (
@@ -9,7 +10,30 @@ export default function Report() {
       <WrapperBox>
         <Calendar />
       </WrapperBox>
-      <div>바 차트 및 음식 추가</div>
+      <div className={clsx(styles.statusWrapper)}>
+        <WrapperBox>
+          <FeedingStatus
+            isAM={true}
+            percentage={20}
+            mealCount={2}
+            maxMealCount={2}
+            snackCount={0}
+            maxSnackCount={1}
+            onAddFood={() => console.log("음식 추가")}
+          />
+        </WrapperBox>
+        <WrapperBox>
+          <FeedingStatus
+            isAM={false}
+            percentage={20}
+            mealCount={2}
+            maxMealCount={2}
+            snackCount={0}
+            maxSnackCount={1}
+            onAddFood={() => console.log("음식 추가")}
+          />
+        </WrapperBox>
+      </div>
       <div>성분 조회 데이터</div>
     </div>
   );
