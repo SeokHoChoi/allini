6903bdc86c190e0269184e261ec802f36de1fe01
seokhochoi <seokho9473@gmail.com>
2024-10-25 16:46:51 +0900

    feat(#63): 간격 조정

--- src/pages/food-tracker/report/index.tsx
+++ src/pages/food-tracker/report/index.tsx
@@ -3,11 +3,30 @@ import clsx from "clsx";
 import styles from "./index.module.scss";
 import WrapperBox from "@ui/wrapperBox";
 import FeedingStatus from "@widgets/report/feedIngStatus";
+import IngredientAnalysis from "@widgets/report/ingredientAnalysis";
+import CharacterSelector from "@ui/characterSelector";
+import { useCharacterSelector } from "@hooks/useCharacterSelector";
+
+const characters = [
+  { path: "mongshil", label: "몽실이" },
+  { path: "boksil", label: "복실이" },
+  { path: "tosil", label: "토실토실토실" },
+];
 
 export default function Report() {
+  const { selectedCharacter, selectCharacter } =
+    useCharacterSelector(characters);
+
   return (
     <div className={clsx(styles.reportArea)}>
-      <WrapperBox>
+      <CharacterSelector
+        className={clsx(styles.selectorArea)}
+        characters={characters}
+        selectedCharacter={selectedCharacter}
+        onSelectCharacter={selectCharacter}
+      />
+
+      <WrapperBox className={clsx(styles.wrappBoxArea)}>
         <Calendar />
       </WrapperBox>
       <div className={clsx(styles.statusWrapper)}>
@@ -34,7 +53,20 @@ export default function Report() {
           />
         </WrapperBox>
       </div>
-      <div>성분 조회 데이터</div>
+      <WrapperBox>
+        {/* color 에 대한 소통 필요 */}
+        <IngredientAnalysis
+        //   ingredients={[
+        //     {
+        //       name: "닭고기",
+        //       percentage: 48,
+        //       color: "#E8805F",
+        //       allergyLevel: "low",
+        //     },
+        //   ]
+        // }
+        />
+      </WrapperBox>
     </div>
   );
 }
